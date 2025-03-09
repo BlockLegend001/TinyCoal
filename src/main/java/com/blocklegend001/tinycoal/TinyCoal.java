b392aa4c2b816e609041780919c1f3c276b8754c
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:58:59 +0100

    Aggiunta 1.21.3-Forge

---
+++ src/main/java/com/blocklegend001/tinycoal/TinyCoal.java
@@ -0,0 +1,28 @@
+package com.blocklegend001.tinycoal;
+
+import com.blocklegend001.tinycoal.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(TinyCoal.MODID)
+public class TinyCoal {
+    public static final String MODID = "tinycoal";
+
+    public TinyCoal(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+        ModItems.register(modEventBus);
+        modEventBus.addListener(this::addCreative);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.TINY_COAL);
+            event.accept(ModItems.TINY_CHARCOAL);
+        }
+    }
+}
