5588723d3bec6918e8d532e0f69de9abafba0a10
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:59:30 +0100

    Aggiunta 1.21.4-Fabric

---
+++ src/main/java/com/blocklegend001/tinycoal/datagen/ModModelProvider.java
@@ -0,0 +1,25 @@
+package com.blocklegend001.tinycoal.datagen;
+
+import com.blocklegend001.tinycoal.item.ModItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.TINY_COAL, Models.GENERATED);
+        itemModelGenerator.register(ModItems.TINY_CHAROAL, Models.GENERATED);
+    }
+}
