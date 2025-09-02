8393e2e0175f4b1a6981374d889cbcedf98167a7
BlockLegend001 <rosarioarcamone@gmail.com>
2025-09-02 15:29:03 +0200

    Fixed recipe conflict with vanilla coal recipe

--- src/main/java/com/blocklegend001/tinycoal/datagen/ModRecipeProvider.java
+++ src/main/java/com/blocklegend001/tinycoal/datagen/ModRecipeProvider.java
@@ -4,6 +4,7 @@ import com.blocklegend001.tinycoal.item.ModItem;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 
 import java.util.concurrent.CompletableFuture;
@@ -18,12 +19,12 @@ public class ModRecipeProvider extends RecipeProvider {
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItem.TINY_COAL.get(), 8)
                 .requires(Items.COAL)
                 .unlockedBy("has_coal", has(Items.COAL))
-                .save(recipeOutput);
+                .save(recipeOutput, String.valueOf(ResourceLocation.fromNamespaceAndPath("tinycoal", "tiny_coal_from_coal")));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItem.TINY_CHARCOAL.get(), 8)
                 .requires(Items.CHARCOAL)
                 .unlockedBy("has_charcoal", has(Items.CHARCOAL))
-                .save(recipeOutput);
+                .save(recipeOutput, String.valueOf(ResourceLocation.fromNamespaceAndPath("tinycoal", "tiny_charcoal_from_charcoal")));
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.COAL)
                 .define('C', ModItem.TINY_COAL)
@@ -31,7 +32,7 @@ public class ModRecipeProvider extends RecipeProvider {
                 .pattern("C C")
                 .pattern("CCC")
                 .unlockedBy("has_tiny_coal", has(ModItem.TINY_COAL.get()))
-                .save(recipeOutput);
+                .save(recipeOutput, String.valueOf(ResourceLocation.fromNamespaceAndPath("tinycoal", "coal_from_tiny_coal")));
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.CHARCOAL)
                 .define('C', ModItem.TINY_CHARCOAL)
@@ -39,6 +40,6 @@ public class ModRecipeProvider extends RecipeProvider {
                 .pattern("C C")
                 .pattern("CCC")
                 .unlockedBy("has_tiny_charcoal", has(ModItem.TINY_CHARCOAL.get()))
-                .save(recipeOutput);
+                .save(recipeOutput, String.valueOf(ResourceLocation.fromNamespaceAndPath("tinycoal", "charcoal_from_tiny_charcoal")));
     }
 }
