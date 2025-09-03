52e487f25a68e55b1455a666a3fe392a6fea408e
BlockLegend001 <rosarioarcamone@gmail.com>
2025-09-03 11:46:40 +0200

    Fixed recipe conflict with vanilla coal recipe

--- src/main/java/com/blocklegend001/tinycoal/datagen/ModRecipeProvider.java
+++ src/main/java/com/blocklegend001/tinycoal/datagen/ModRecipeProvider.java
@@ -4,6 +4,7 @@ import com.blocklegend001.tinycoal.item.ModItem;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 
 import java.util.concurrent.CompletableFuture;
@@ -34,12 +35,12 @@ public class ModRecipeProvider extends RecipeProvider {
         shapeless(RecipeCategory.MISC, ModItem.TINY_COAL.get(), 8)
                 .requires(Items.COAL)
                 .unlockedBy("has_coal", has(Items.COAL))
-                .save(this.output);
+                .save(this.output, String.valueOf(ResourceLocation.fromNamespaceAndPath("tinycoal", "tiny_coal_from_coal")));
 
         shapeless(RecipeCategory.MISC, ModItem.TINY_CHARCOAL.get(), 8)
                 .requires(Items.CHARCOAL)
                 .unlockedBy("has_charcoal", has(Items.CHARCOAL))
-                .save(this.output);
+                .save(this.output, String.valueOf(ResourceLocation.fromNamespaceAndPath("tinycoal", "tiny_charcoal_from_charcoal")));
 
         shaped(RecipeCategory.MISC, Items.COAL)
                 .define('C', ModItem.TINY_COAL)
@@ -47,7 +48,7 @@ public class ModRecipeProvider extends RecipeProvider {
                 .pattern("C C")
                 .pattern("CCC")
                 .unlockedBy("has_tiny_coal", has(ModItem.TINY_COAL.get()))
-                .save(this.output);
+                .save(this.output, String.valueOf(ResourceLocation.fromNamespaceAndPath("tinycoal", "coal_from_tiny_coal")));
 
         shaped(RecipeCategory.MISC, Items.CHARCOAL)
                 .define('C', ModItem.TINY_CHARCOAL)
@@ -55,7 +56,7 @@ public class ModRecipeProvider extends RecipeProvider {
                 .pattern("C C")
                 .pattern("CCC")
                 .unlockedBy("has_tiny_charcoal", has(ModItem.TINY_CHARCOAL.get()))
-                .save(this.output);
+                .save(this.output, String.valueOf(ResourceLocation.fromNamespaceAndPath("tinycoal", "charcoal_from_tiny_charcoal")));
     }
 
 }
